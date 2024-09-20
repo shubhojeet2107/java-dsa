c81e082de1dd09ff6776eeb059c881e09877a99b
shubhojeet2107 <shubhojeetnayak5220@gmail.com>
2024-09-20 10:37:25 +0530

    .

--- java_2Darrays/sumofdiagonal.java
+++ java_2Darrays/sumofdiagonal.java
@@ -3,39 +3,39 @@
 package java_2Darrays;
 
 import java.util.Scanner;
-public class sumofdiagonal {
-        public static int diagonalSum(int[][] mat) {
-            int n = mat.length;
-            int sum = 0;
-    
-            for (int i = 0; i < n; i++) {
-                // Add primary diagonal element
-                sum += mat[i][i];
-    
-                // Add secondary diagonal element, avoid double counting of center element
-                if (i != n - i - 1) {
-                    sum += mat[i][n - i - 1];
-                }
-            }
-            return sum;
-        }
-    
-        public static void main(String[] args) {
-            try (Scanner sc = new Scanner(System.in)) {
-                System.out.print("Enter the number of rows: ");
-                int rows = sc.nextInt();
-                System.out.print("Enter the number of coloumns: ");
-                int cols = sc.nextInt();
-                int[][] mat = new int[rows][cols];
 
-                // Taking Inputs
-                for(int i=0; i<rows; i++){
-                    for(int j=0; j<cols; j++){
-                        mat[i][j] = sc.nextInt();
-                    }
-                }
-                System.out.println("Diagonal sum: " + diagonalSum(mat));
+public class sumofdiagonal {
+    public static int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Add primary diagonal element
+            sum += mat[i][i];
+
+            // Add secondary diagonal element, avoid double counting of center element
+            if (i != n - i - 1) {
+                sum += mat[i][n - i - 1];
             }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the number of rows: ");
+            int rows = sc.nextInt();
+            System.out.print("Enter the number of coloumns: ");
+            int cols = sc.nextInt();
+            int[][] mat = new int[rows][cols];
+
+            // Taking Inputs
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    mat[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println("Diagonal sum: " + diagonalSum(mat));
         }
     }
-    
+}
