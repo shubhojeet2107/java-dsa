139885e33c3ca189cd79e66b3c48179b173e6451
shubhojeet2107 <shubhojeetnayak5220@gmail.com>
2024-10-04 19:47:32 +0530

    .

--- Java_arrays/findmin.java
+++ Java_arrays/findmin.java
@@ -2,6 +2,30 @@
 
 package java_arrays;
 
+import java.util.Scanner;
 public class findmin {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] nums = new int[size];
+
+        // Taking Input from the user
+        for(int i=0; i<nums.length; i++){
+            System.out.print("Number " +(i+1)+ ": ");
+            nums[i] = sc.nextInt();
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        // Output
+        for(int i=0; i<nums.length; i++){
+            if (min > nums[i]) {
+                min = nums[i];
+            }
+        }
+        System.out.print("Smallest Element: " +min);
+        sc.close();
+    }
 }
