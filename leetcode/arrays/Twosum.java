f3d46698f1120709c864719ec6d6d40ace81b350
shubhojeet2107 <shubhojeetnayak5220@gmail.com>
2024-10-05 19:57:49 +0530

    .

--- leetcode/arrays/Twosum.java
+++ leetcode/arrays/Twosum.java
@@ -31,7 +31,7 @@ public class Twosum {
         // int[] ans = new int[size];
     
         for(int i=0; i<nums.length; i++){
-            for(int j=(i+1); j<nums.length; j++){
+            for(int j=nums.length-1; j>i; j--){
                 if(nums[i]+ nums[j] == target){
                     // ans[i] = {nums[i], nums[j]};
                     return new int[]{i, j};
