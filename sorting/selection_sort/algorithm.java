28c999e19728f008d3a031914f7036d529348df8
shubhojeet2107 <shubhojeetnayak5220@gmail.com>
2024-10-20 07:34:16 +0530

    .

---
+++ sorting/selection_sort/algorithm.java
@@ -0,0 +1,35 @@
+package sorting.selection_sort;
+
+import java.util.Arrays;
+
+public class algorithm {
+    public static void main(String[] args){
+        int[] nums = {5,4,3,2,1};
+        selection(nums);
+        System.out.print(Arrays.toString(nums));
+    }
+
+    public static void selection(int[] nums){
+        for(int i=0; i<nums.length; i++){
+            int last = nums.length-i-1;
+            int maxIndex = getMaxIndex(nums, 0, last);
+            swap(nums, maxIndex, last);
+        }
+    }
+
+    public static void swap(int[] nums, int first, int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+
+    public static int getMaxIndex(int[] nums, int start, int end){
+        int max = start;
+        for(int i=start; i<=end; i++){
+            if(nums[max] < nums[i]){
+                max = i;
+            }
+        }
+        return max;
+    }
+}
