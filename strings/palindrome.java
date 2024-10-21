f083e171d120e025cc3ce8f9fb3c06f2ddf2f3d6
shubhojeet2107 <shubhojeetnayak5220@gmail.com>
2024-10-21 17:12:47 +0530

    check for palindrome

---
+++ strings/palindrome.java
@@ -0,0 +1,23 @@
+package strings;
+
+public class palindrome {
+    public static void main(String[] args){
+        String str = "abccba";
+        System.out.println(isPalindrome(str));
+    }
+
+    private static boolean isPalindrome(String str) {
+        str = str.toLowerCase();
+        int start = 0;
+        int end = str.length()-1;
+        while(start < end){
+            if (str.charAt(start) != str.charAt(end)){
+                return false;
+            }else{
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
