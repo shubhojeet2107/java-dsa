4933aff89119beb1b28ebea397e7d6106f3afde9
shubhojeet2107 <shubhojeetnayak5220@gmail.com>
2024-10-27 12:47:40 +0530

    recursion

--- recursion/sumtrianglefromarray.java
+++ recursion/sumtrianglefromarray.java
@@ -1,5 +1,30 @@
 package recursion;
 
+import java.util.Arrays;
+
 public class sumtrianglefromarray {
-    
+    public static void main(String[] args){
+        int nums[] = {1, 2, 3, 4, 5};
+        sum(nums);
+    }
+
+    public static void sum(int[] nums){
+        
+        // Base case: if only one element is left, print it
+        if(nums.length < 1){
+            return;
+        }
+
+        // Create a new array to store the sum of adjacent elements
+        int[] ans = new int[nums.length-1];
+        for(int i=0; i<nums.length-1; i++){
+            ans[i] = nums[i] + nums[i+1];
+        }
+
+        // Recursive call with the smaller array
+        sum(ans);
+
+        // Print the current array
+        System.out.println(Arrays.toString(nums));
+    }
 }
