72b7b32aa9be3add7a7d72787e89a103648896ef
shubhojeet2107 <shubhojeetnayak5220@gmail.com>
2024-10-30 02:29:54 +0530

    correction

--- sorting/cyclic_sort/algorithm.java
+++ sorting/cyclic_sort/algorithm.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class algorithm {
     public static void main(String[] args) {
-        int[] nums = {2,0,2,1,1,0};
+        int[] nums = {5, 4, 3, 2, 1};
         cycle(nums);
         System.out.print(Arrays.toString(nums));
     }
@@ -12,7 +12,7 @@ public class algorithm {
     static void cycle(int[] nums) {
         int i = 0;
         while (i < nums.length) {
-            int correct = (nums[i]);
+            int correct = (nums[i]-1);
             if (nums[i] != nums[correct]) {
                 swap(nums, i, correct);
             } else {
