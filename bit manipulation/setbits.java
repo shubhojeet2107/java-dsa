6a5ee395ab629484facb332be85c96b84d5f9533
shubhojeet2107 <shubhojeetnayak5220@gmail.com>
2024-10-29 21:00:31 +0530

    bitwise operations

---
+++ bit manipulation/setbits.java
@@ -0,0 +1,17 @@
+// num = 9
+// num in binary = 1001; thus ans = 2;
+
+public class setbits {
+    public static void main(String[] args){
+        int num = 9;
+        int count = 0;
+
+        while (num > 0){
+            if((num & 1) == 1){
+                count++;
+            } 
+            num = num >> 1;
+        }
+        System.out.print(count);
+    }
+}
