b324d7dc98b61f4ee4b2949f2a7e472403bd5bd1
shubhojeet2107 <shubhojeetnayak5220@gmail.com>
2024-10-29 21:03:26 +0530

    changes made

--- bit manipulation/setbits.java
+++ bit manipulation/setbits.java
@@ -5,13 +5,15 @@ public class setbits {
     public static void main(String[] args){
         int num = 9;
         int count = 0;
-
+        System.out.println(Integer.toBinaryString(num));
+        
         while (num > 0){
             if((num & 1) == 1){
                 count++;
             } 
             num = num >> 1;
         }
+        // System.out.println(Integer.toBinaryString(num));
         System.out.print(count);
     }
 }
