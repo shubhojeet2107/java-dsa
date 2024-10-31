9cba14ba6a564bc7a2a69de6efa26921d83a4242
shubhojeet2107 <shubhojeetnayak5220@gmail.com>
2024-10-31 13:32:33 +0530

    method of sieve

--- maths/seive.java
+++ maths/seive.java
@@ -3,7 +3,25 @@
 package maths;
 
 public class seive {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        int num = 40;
+        boolean[] prime = new boolean[num + 1];
+        isPrime(num, prime);
+    }
+
+    public static void isPrime(int num, boolean[] prime) {
+
+        for (int i = 2; i * i <= num; i++) {
+            if (!prime[i]) {
+                for (int j = i * 2; j <= num; j += i) {
+                    prime[j] = true;
+                }
+            }
+        }
+        for (int i = 2; i <= num; i++) {
+            if (!prime[i]) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
--- strings/practice.java
+++ strings/practice.java
@@ -1,7 +1,5 @@
 package strings;
 
-import java.util.Arrays;
-
 public class practice {
     public static void main(String[] args){
         String a = "1010";
