62853a463e5e7ae6ee465b790aa09ae5f4dcc8cf
Shubhojeet Nayak <147873545+shubhojeet2107@users.noreply.github.com>
2024-12-29 15:42:35 +0530

    Sap interview

---
+++ practice/teams.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class teams {
+    public static void main (String[] args){
+        int[] nums = {11,13,17, 24, 33, 41, 10, 22, 34};
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] > first){
+                second = first;
+                first = nums[i];
+            }else{
+                second = nums[i];
+            }
+        }
+        if(second == Integer.MIN_VALUE){
+            System.out.println("NO Second largest");
+        }else{
+            System.out.println(second);
+        }
+    }
+}
