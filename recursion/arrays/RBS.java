c03bf322967901dba266c92d2e71e8a10146b45b
Shubhojeet Nayak <147873545+shubhojeet2107@users.noreply.github.com>
2025-02-16 16:26:25 +0530

    Rotated binary search with recursion

---
+++ recursion/arrays/RBS.java
@@ -0,0 +1,35 @@
+package recursion.arrays;
+
+public class RBS {
+    public static void main(String[] args){
+        int[] arr = {5, 6, 7, 8, 9, 1, 2, 3};
+        int ans = search(arr, 2, 0, arr.length-1);
+        System.out.print(ans);
+    }
+
+    public static int search(int[] arr, int target, int start, int end){
+
+        if(start > end){
+            return -1;
+        }
+
+        int mid = start+(end-start)/2;
+
+        if(arr[mid] == target){
+            return mid;
+        }
+
+        if(arr[start] < arr[mid]){
+            if(target < arr[mid] && target > arr[start]){
+                return search(arr, target, start, mid-1);
+            }else{
+                return search(arr, target, mid+1, end);
+            }
+        }
+
+        if(target > arr[mid] && arr[end] > target){
+            return search(arr, target, mid+1, end);
+        }
+        return search(arr, target, start, mid-1);
+    }
+}
