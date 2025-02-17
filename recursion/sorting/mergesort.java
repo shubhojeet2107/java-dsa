09442d8123f0028c1cdfe3e4af56490111a67e4e
Shubhojeet Nayak <147873545+shubhojeet2107@users.noreply.github.com>
2025-02-17 16:45:04 +0530

    Merge Sort

---
+++ recursion/sorting/mergesort.java
@@ -0,0 +1,57 @@
+package recursion.sorting;
+
+import java.util.Arrays;
+
+public class mergesort {
+    public static void main(String[] args){
+        int[] arr = {5, 4, 3, 2, 1};
+        int[] ans = mergeSort(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    public static int[] mergeSort(int[] arr){
+        if(arr.length == 1){
+            return arr;
+        }
+
+        int mid = arr.length/2;
+
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+
+    public static int[] merge(int[] firstHalf, int[] secondHalf){
+        int mix[] = new int [firstHalf.length + secondHalf.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i < firstHalf.length && j < secondHalf.length){
+            if(firstHalf[i] < secondHalf[j]){
+                mix[k] = firstHalf[i];
+                i++;
+            }else{
+                mix[k] = secondHalf[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < firstHalf.length){
+            mix[k] = firstHalf[i];
+            k++;
+            i++;
+        } 
+
+        while(j < secondHalf.length){
+            mix[k] = secondHalf[j];
+            j++;
+            k++;
+        }
+
+        return mix;
+    }
+}
