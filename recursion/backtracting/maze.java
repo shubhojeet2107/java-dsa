987a880acca658e53a21016ad0f048cfff5a5e2b
Shubhojeet Nayak <147873545+shubhojeet2107@users.noreply.github.com>
2025-03-03 18:45:41 +0530

    This contains implementations of various maze-related problems

--- recursion/backtracting/maze.java
+++ recursion/backtracting/maze.java
@@ -4,14 +4,21 @@ package recursion.backtracting;
 
 public class maze {
     public static void main(String[] args){
-        System.out.println(problem1(3, 3));
-        System.out.println();
-        problem2("", 3, 3);
-        System.out.println();
-        problem3("", 3, 3);
+        // System.out.println(problem1(3, 3));
+        // System.out.println();
+        // problem2("", 3, 3);
+        // System.out.println();
+        // problem3("", 3, 3);
+
+        boolean[][] game = {
+            {true, true, true},
+            {true, false, true},
+            {true, true, true},
+        };
+        problem4("", game, 0, 0);
     }
 
-    // Problem1: Count the number of ways to reach the bottom-right corner from the top-left corner in a grid, moving only right or down.
+    // Problem 1: Count the number of ways to reach the bottom-right corner from the top-left corner in a grid, moving only right or down.
     public static int problem1(int row, int col){
         if(row == 1 || col == 1){
             return 1;
@@ -23,7 +30,7 @@ public class maze {
         return left+right;
     }
 
-    //Problem2: Print the number of ways to reach the bottom-right corner from the top-left corner in a grid, moving only right or down.
+    //Problem 2: Print the number of ways to reach the bottom-right corner from the top-left corner in a grid, moving only right or down.
     public static void problem2(String p, int row, int col){
         if(row == 1 && col == 1){
             System.out.println(p);
@@ -39,7 +46,7 @@ public class maze {
         }
     }
 
-    //Problem3: Print all possible paths to reach the bottom-right corner from the top-left corner in a grid, moving right ('R'), down ('D'), or diagonally ('d').
+    //Problem 3: Print all possible paths to reach the bottom-right corner from the top-left corner in a grid, moving right ('R'), down ('D'), or diagonally ('d').
     public static void problem3(String p, int row, int col){
         if(row == 1 && col == 1){
             System.out.println(p);
@@ -56,6 +63,26 @@ public class maze {
 
         if(col > 1){
             problem3(p+'R', row, col-1);
+        }
+    }
+
+    // Problem 4: Print all possible paths to reach the bottom-right corner from the top-left corner in a grid, moving right ('R'), down ('D') with an obstacle in any of the block of a grid
+    public static void problem4(String p, boolean[][] maze, int row, int col){
+        if(row == maze.length-1 && col == maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+
+        if(!maze[row][col]){
+            return;
+        }
+
+        if(row < maze.length-1){
+            problem4(p+'D', maze, row+1, col);
+        }
+
+        if(col < maze[0].length-1){
+            problem4(p+'R', maze, row, col+1);
         }
     }
 }
