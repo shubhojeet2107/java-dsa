0e29b5a1a32a8ec8c915bdc7c325f0de94d4a26c
Shubhojeet Nayak <147873545+shubhojeet2107@users.noreply.github.com>
2025-08-01 19:47:00 +0530

    Implementation of Binary Tree

---
+++ trees/ImplementBT.java
@@ -0,0 +1,86 @@
+package trees;
+
+import java.util.Scanner;
+
+public class ImplementBT {
+
+    public ImplementBT(){
+    }
+
+    private static class Node{
+        private int value;
+        private Node left;
+        private Node right;
+
+        private Node(int value){
+            this.value = value;
+        }
+    }
+
+    // insert elements
+    // Creation of Root Node.
+    public Node root;
+    public void setRoot(Scanner sc){
+        System.out.print("Enter the Root Node: ");
+        int value = sc.nextInt();
+        root = new Node(value);
+        populate(sc, root);
+    }
+
+    // Creation of left Node and Right Node.
+    public void populate(Scanner sc, Node node){
+        System.out.print("Do you want to enter left of " +node.value +": ");
+        boolean left = sc.nextBoolean();
+        if(left){
+            System.out.print("Enter the value of left of " +node.value +": ");
+            int value = sc.nextInt();
+            node.left = new Node(value);
+            populate(sc, node.left);
+        }
+
+        System.out.print("Do you want to enter the right of " +node.value +": ");
+        boolean right = sc.nextBoolean();
+        if(right){
+            System.out.print("Enter the value of right of " +node.value +": ");
+            int value = sc.nextInt();
+            node.right = new Node(value);
+            populate(sc, node.right);
+        }
+    }
+
+    // Displaying the Tree:
+    public void display(){
+        display(this.root, "");
+    }
+    public void display(Node node, String indent){
+        if(node == null){
+            return;
+        }
+        System.out.println(indent + node.value);
+        display(node.left, indent+"\t");
+        display(node.right, indent+"\t");
+    }
+
+    // Pretty Display:
+    public void prettyDisplay() {
+        prettyDisplay(root, 0);
+    }
+
+    private void prettyDisplay(Node node, int level) {
+        if (node == null) {
+            return;
+        }
+
+        prettyDisplay(node.right, level + 1);
+
+        if (level != 0) {
+            for (int i = 0; i < level - 1; i++) {
+                System.out.print("|\t\t");
+            }
+            System.out.println("|------->" + node.value);
+        } else {
+            System.out.println(node.value);
+        }
+        prettyDisplay(node.left, level + 1);
+    }
+}
